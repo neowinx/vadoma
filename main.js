1ddcdeb6cd4df9cf6960ac0efa976e3d1b873fa3
Pedro Flores <pflores@dncp.gov.py>
2017-12-14 10:36:21 -0300

    Added elastic post code

--- main.js
+++ main.js
@@ -20,6 +20,20 @@ async function processData(url) {
             json: true
         });
 
+        let elasticUrl = `${config.elastic.url}/${config.elastic.index}/${config.elastic.type}`
+
+        console.log(`sending response to ${chalk.yellow(elasticUrl)}`);
+
+        await rp({
+            url: `${config.elastic.url}/${config.elastic.index}/${config.elastic.type}`,
+            method: 'POST',
+            headers: {
+                'Authorization': `Basic ${base64.encode(`${config.elastic.username}:${config.elastic.password}`)}`
+            },
+            json: true,
+            body: body
+        });
+
         if(body && body.d && body.d.__next) {
             let nextUrl = body.d.__next
             console.log(`processing next page ${chalk.yellow(nextUrl)}`);
